a7de755a30d04820f2cd351cd093ce54f4c2d65a
Robbie Ballou <ballourl@mail.uc.edu>
2024-10-28 19:17:58 -0400

    Initial UI Push

---
+++ src/main/java/com/exercisenow/enterprise/ExerciseController.java
@@ -0,0 +1,14 @@
+package com.exercisenow.enterprise;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ExerciseController {
+
+    @RequestMapping("/")
+    public string index() {
+        return "start";
+    }
+
+}
