9f64f0e76f735e9cd7c432474bdf8a486a83e2a5
Drew <milukaj@mail.uc.edu>
2024-11-03 21:32:36 -0500

    -Added a onclick logEvent for whenever a workout is started

--- src/main/resources/templates/script.js
+++ src/main/resources/templates/script.js
@@ -2,3 +2,8 @@ const toggler = document.querySelector(".toggler-btn")
 toggler.addEventListener("click", function(){
     document.querySelector("#sidebar").classList.toggle("collapsed")
 })
+
+function logEvent(action, details = "") {
+    const timestamp = new Date().toLocaleString();
+    console.log(`[${timestamp}] ${action}: ${details}`);
+}
